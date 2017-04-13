f1d5b8e16af4778b3e317bb917ea976a326af54e
e04440 <e04440@W8418371.rmit.internal>
2017-04-13 12:23:34 +1000

    Added line to Polygon

---
+++ src/org/knime/geo/linetopoly/LineToPolygonNodeView.java
@@ -0,0 +1,48 @@
+package org.knime.geo.linetopoly;
+
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeView</code> for the "LineToPolygon" Node.
+ * 
+ *
+ * @author Forkan
+ */
+public class LineToPolygonNodeView extends NodeView<LineToPolygonNodeModel> {
+
+    /**
+     * Creates a new view.
+     * 
+     * @param nodeModel The model (class: {@link LineToPolygonNodeModel})
+     */
+    protected LineToPolygonNodeView(final LineToPolygonNodeModel nodeModel) {
+        super(nodeModel);
+        // TODO: generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void modelChanged() {
+        // TODO: generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onClose() {
+        // TODO: generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onOpen() {
+        // TODO: generated method stub
+    }
+
+}
+
